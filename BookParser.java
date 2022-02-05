58e29cfb966a91a9c149f499ece98268ea768a67
Charles Nykamp <cqnykamp@gmail.com>
2022-02-05 13:52:48 -0600

    Create list of words and count, write to file

--- BookParser.java
+++ BookParser.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 
 import java.util.HashMap;
 
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+
 public class BookParser {
 
     public static void main(String[] args) {
@@ -18,13 +22,29 @@ public class BookParser {
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String[] split = myReader.nextLine().split(" ");
+
+                String extraPunct = "!@#$%^&*()+~,.?\'\"[]";
                 
                 for(String word : split) {
-                    if(words.containsKey(word)) {
-                        words.put(word, words.get(word) + 1);
-                    } else {
-                        words.put(word, 1);
+                    String newWord = "";
+                    for(int i=0; i<word.length(); i++) {
+                        char c = word.charAt(i);
+                        if(extraPunct.indexOf(c) != -1) {
+
+                        } else {
+                            newWord += c;
+                        }
                     }
+
+                    if(newWord != "") {
+                        if(words.containsKey(newWord)) {
+                            words.put(newWord, words.get(newWord) + 1);
+                        } else {
+                            words.put(newWord, 1);
+                        }
+
+                    }
+
                 }
                 // System.out.println(data);
             }
@@ -34,9 +54,23 @@ public class BookParser {
             e.printStackTrace();
         }
 
-        for(String word : words.keySet()) {
-            System.out.println(word + " " + words.get(word));
+        try {
+            FileWriter myWriter = new FileWriter("words.txt");
+            // myWriter.write("Files in Java might be tricky, but it is fun enough!");
+
+            for(String word : words.keySet()) {
+                myWriter.write(word + " " + words.get(word) + "\n");
+                // System.out.println();
+            }
+
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
+
+
     }
     
 }
